e8e64403f97209f671a462f59791738177e423cc
“Natalia <natalia.rudiger@gmail.com>
2022-02-05 15:47:43 -0200

    passo 3 e 4 completos

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,38 @@
 const textInputArea = document.getElementById('text-input-area');
 const buttonAdd = document.getElementById('button-addon2');
-const buttonElements = document.getElementById('button-elements');
+const ulElements = document.getElementById('ul'); 
+
+
+function insertListItem() {
+    const li = `
+    <li class="list-group-item d-flex justify-content-between">
+      ${textInputArea.value}
+      <button class="btn btn btn-danger">-</button>
+    </li>`;
+  
+    ulElements.insertAdjacentHTML('beforeend', li);
+  }
+  
+  buttonAdd.addEventListener('click', () => {
+    
+    insertListItem();
+  
+    textInputArea.value = "";
+  
+    textInputArea.focus();
+  });
+
+  document.addEventListener("click", (event) => {
+    const deleteButtonElement = event.target;
+  
+   
+    if (deleteButtonElement.classList.contains("btn-danger")) {
+      
+  
+      const li = deleteButtonElement.parentElement;
+  
+      ulElements.removeChild(li);
+    }
+  });
+
 
