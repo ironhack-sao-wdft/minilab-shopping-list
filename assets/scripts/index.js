d933f5a4f6f1efe1a192fd28926f84281d82e40c
Bruno Wake <brunowake.bw@gmail.com>
2022-02-05 11:43:18 -0300

    using event delegation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,12 @@ const listArr = ["Banana", "Pão", "Acúcar", "Café"];
 
 ul.addEventListener("load", renderList(listArr));
 
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("btn-danger")) {
+    removeItem(event.target.parentNode.innerText.replace(/\n-/, ""));
+  }
+});
+
 addBtn.addEventListener("click", (_) => {
   addItem(addItemText.value);
   addItemText.value = "";
@@ -16,14 +22,15 @@ function addItem(item) {
   renderList(listArr);
 }
 function removeItem(item) {
-  listArr.splice(listArr.indexOf(item.toString()), 1);
+  listArr.splice(listArr.indexOf(item), 1);
   renderList(listArr);
 }
 
 function renderList(arr) {
   ul.innerHTML = "";
   arr.map((current, index) => {
-    const li = `<li id='${index}' class="list-group-item d-flex justify-content-between">${current} <button class="btn btn btn-danger" onclick= "removeItem('${current}')" >-</button> </li>`;
+    // const li = `<li id='${index}' class="list-group-item d-flex justify-content-between">${current} <button class="btn btn btn-danger" onclick= "removeItem('${current}')" >-</button> </li>`;
+    const li = `<li id='${index}' class="list-group-item d-flex justify-content-between">${current} <button class="btn btn btn-danger"  >-</button> </li>`;
     ul.insertAdjacentHTML("beforeend", li);
   });
 }
