fb2c922d0db57fdf79ead2b4d4e21d2b65fb3816
“Natalia <natalia.rudiger@gmail.com>
2022-02-05 00:38:33 -0200

    passo 2 completo - seletores

---
+++ assets/scripts/index.js
@@ -0,0 +1,4 @@
+const textInputArea = document.getElementById('text-input-area');
+const buttonAdd = document.getElementById('button-addon2');
+const buttonElements = document.getElementById('button-elements');
+
